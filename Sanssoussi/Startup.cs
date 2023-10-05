0603deacb680a04350307fe15e9a6f101c5f6cc1
assassimb <assassimb@gmail.com>
2023-10-05 15:45:38 -0400

    added cors

--- Sanssoussi/Startup.cs
+++ Sanssoussi/Startup.cs
@@ -18,6 +18,16 @@ namespace Sanssoussi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "Corsingtime",
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("https://localhost:5001")
+                                             .AllowAnyHeader()
+                                             .AllowAnyMethod();
+                                  });
+            });
             services.AddRazorPages();
             services.AddControllersWithViews();
         }
@@ -35,6 +45,9 @@ namespace Sanssoussi
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();  // This middleware adds the Strict-Transport-Security header to HTTP responses.
             }
+
+            app.UseCors("Corsingtime");  // Apply the CORS policy
+
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
